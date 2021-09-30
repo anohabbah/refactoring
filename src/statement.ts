9a57eb455fb1787dcf834621f36d0d826b25632b
anohabbah <anohabbah@gmail.com>
2021-09-30 17:35:17 +0200

    extract volume credits

--- src/statement.ts
+++ src/statement.ts
@@ -9,11 +9,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
     result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
@@ -51,5 +47,12 @@ function statement(invoice, plays) {
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
+  }
+
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    if ('comedy' === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+    return volumeCredits;
   }
 }
